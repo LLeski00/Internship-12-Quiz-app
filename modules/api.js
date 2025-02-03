82e82f26df8986fea0f497a2dc14d1c84354021a
Luka <llesko00@fesb.hr>
2025-02-03 21:29:55 +0100

    Fetch questions from API

--- modules/api.js
+++ modules/api.js
@@ -1,11 +1,24 @@
-function getTriviaQuestions(category, difficulty, gameType) {
-    let apiUrl = "https://opentdb.com/api_config.php?amount=5";
+async function getTriviaQuestions(category, difficulty, gameType) {
+    let apiUrl = "https://opentdb.com/api.php?amount=5";
+    let questions;
 
     apiUrl = addQueryParameter(apiUrl, category);
     apiUrl = addQueryParameter(apiUrl, difficulty);
     apiUrl = addQueryParameter(apiUrl, gameType);
 
-    console.log(apiUrl);
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        questions = json.results;
+    } catch (error) {
+        console.error(error.message);
+    }
+
+    console.log(questions);
 }
 
 function addQueryParameter(apiUrl, parameter) {
